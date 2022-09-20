1a391638d0406177c5a4b33f76040dc32aaa957e
Brian Mayo <ioprotium@gmail.com>
2022-09-20 23:10:35 +0200

    feat: implement String() for statments

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "protiumx.dev/simia/token"
+import (
+	"strings"
+
+	"protiumx.dev/simia/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -27,6 +32,10 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -39,6 +48,19 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out strings.Builder
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 type ReturnStatment struct {
 	Token       token.Token
 	ReturnValue Expression
@@ -48,6 +70,34 @@ func (rs *ReturnStatment) statementNode() {}
 
 func (rs *ReturnStatment) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatment) String() string {
+	var out strings.Builder
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(" = ")
+	return out.String()
+}
+
+type ExpressionStatment struct {
+	Token      token.Token // firs token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatment) statementNode() {}
+
+func (es *ExpressionStatment) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatment) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
 
 // Root node
@@ -64,4 +114,12 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 	return p.Statements[0].TokenLiteral()
+}
+
+func (p *Program) String() string {
+	var out strings.Builder
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
