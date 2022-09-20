541f400c7884ca62b6a6ea6a925cea7263ec55a7
Brian Mayo <ioprotium@gmail.com>
2022-09-20 17:18:45 +0200

    feat: wip AST

---
+++ ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+import "protiumx.dev/simia/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Root node
+type Program struct {
+	Statements []Statement
+}
+
+func NewProgram() *Program {
+	return &Program{Statements: []Statement{}}
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) == 0 {
+		return ""
+	}
+	return p.Statements[0].TokenLiteral()
+}
