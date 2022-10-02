765397193af0abfb7c897ef78726ecc71d41ee3e
Brian Mayo <ioprotium@gmail.com>
2022-10-02 02:22:13 +0200

    refactor: use evaluator. wip

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"protiumx.dev/simia/evaluator"
 	"protiumx.dev/simia/lexer"
 	"protiumx.dev/simia/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
