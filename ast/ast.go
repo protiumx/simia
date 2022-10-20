462992b5282c61b2a48fbc83417b13b86dcfc728
Brian Mayo <ioprotium@gmail.com>
2022-10-20 12:27:28 +0200

    feat: added STRING token and ast

--- ast/ast.go
+++ ast/ast.go
@@ -66,6 +66,21 @@ func (b *Boolean) String() string {
 	return b.Token.Literal
 }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+
+func (sl *StringLiteral) TokenLiteral() string {
+	return sl.Token.Literal
+}
+
+func (sl *StringLiteral) String() string {
+	return sl.Token.Literal
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
