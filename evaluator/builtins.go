9e2177951690b21774ded5700912250b3d448dfa
Brian Mayo <ioprotium@gmail.com>
2022-10-24 23:35:36 +0200

    feat: added append fn for arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -5,16 +5,54 @@ import "protiumx.dev/simia/value"
 var builtins = map[string]*value.Builtin{
 	"len": {
 		Fn: func(args ...value.Value) value.Value {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			if err := checkArgsNumber(1, args); err != nil {
+				return err
 			}
 
 			switch arg := args[0].(type) {
 			case *value.String:
 				return &value.Integer{Value: int64(len(arg.Value))}
+			case *value.Array:
+				return &value.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", arg.Type())
 			}
 		},
 	},
+	"append": {
+		Fn: func(args ...value.Value) value.Value {
+			if err := checkArgsNumber(2, args); err != nil {
+				return err
+			}
+
+			if err := checkArgType(args[0].Type(), value.ARRAY_VALUE, "append"); err != nil {
+				return err
+			}
+
+			arr := args[0].(*value.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]value.Value, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &value.Array{Elements: newElements}
+		},
+	},
+}
+
+func checkArgsNumber(n int, args []value.Value) *value.Error {
+	if len(args) != n {
+		return newError("wrong number of arguments. got=%d, want=%d", len(args), n)
+	}
+
+	return nil
+}
+
+func checkArgType(argType value.ValueType, expectedType value.ValueType, fnName string) *value.Error {
+	if argType.Type() != expectedType {
+		return newError("argument to `%s` must be %s, got %s", fnName, argType.Type(), expectedType)
+	}
+
+	return nil
 }
