9bcafd896515fa100b908ff5628f361fe127ab39
Brian Mayo <ioprotium@gmail.com>
2022-11-15 16:35:06 +0100

    feat: added wasm module

---
+++ cmd/wasm/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"syscall/js"
+
+	"protiumx.dev/simia/evaluator"
+	"protiumx.dev/simia/lexer"
+	"protiumx.dev/simia/parser"
+	"protiumx.dev/simia/value"
+)
+
+var Version = ""
+
+var env = value.NewEnvironment(nil)
+
+func wrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return "ERROR: invalid arguments"
+		}
+
+		code := args[0].String()
+		l := lexer.New(code)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if errs := p.Errors(); len(errs) != 0 {
+			return errs[len(errs)-1]
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			return evaluated.Inspect()
+		}
+
+		return ""
+	})
+	return jsonFunc
+}
+
+func main() {
+	js.Global().Set("simia", wrapper())
+	js.Global().Set("simia_version", js.ValueOf(Version))
+	<-make(chan struct{})
+}
