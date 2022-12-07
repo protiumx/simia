e90747fada043383088ce3959ca30775649fc65f
Brian Mayo <ioprotium@gmail.com>
2022-12-07 01:14:43 +0100

    feat: added jump op

--- code/code.go
+++ code/code.go
@@ -63,6 +63,9 @@ const (
 
 	OpMinus
 	OpBang
+
+	OpJumpIfBranch
+	OpJump
 )
 
 type Definition struct {
@@ -71,19 +74,21 @@ type Definition struct {
 }
 
 var definitions = map[Opcode]*Definition{
-	OpConstant:    {"OpConstant", []int{2}}, // Const has size uint16 (2bytes)
-	OpAdd:         {"OpAdd", []int{}},
-	OpPop:         {"OpPop", []int{}},
-	OpSub:         {"OpSub", []int{}},
-	OpMul:         {"OpMul", []int{}},
-	OpDiv:         {"OpDiv", []int{}},
-	OpTrue:        {"OpTrue", []int{}},
-	OpFalse:       {"OpFalse", []int{}},
-	OpEqual:       {"OpEqual", []int{}},
-	OpNotEqual:    {"OpNotEqual", []int{}},
-	OpGreaterThan: {"OpGreaterThan", []int{}},
-	OpMinus:       {"OpMinus", []int{}},
-	OpBang:        {"OpBang", []int{}},
+	OpConstant:     {"OpConstant", []int{2}}, // Const has size uint16 (2bytes)
+	OpAdd:          {"OpAdd", []int{}},
+	OpPop:          {"OpPop", []int{}},
+	OpSub:          {"OpSub", []int{}},
+	OpMul:          {"OpMul", []int{}},
+	OpDiv:          {"OpDiv", []int{}},
+	OpTrue:         {"OpTrue", []int{}},
+	OpFalse:        {"OpFalse", []int{}},
+	OpEqual:        {"OpEqual", []int{}},
+	OpNotEqual:     {"OpNotEqual", []int{}},
+	OpGreaterThan:  {"OpGreaterThan", []int{}},
+	OpMinus:        {"OpMinus", []int{}},
+	OpBang:         {"OpBang", []int{}},
+	OpJumpIfBranch: {"OpJumpBranch", []int{2}},
+	OpJump:         {"OpJump", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
